b3177c048597267466307688c4d5c576b6fa20c9
roms2014 <tishenkoroma@icloud.com>
2023-03-10 15:19:55 +0300

    Sounds hot_deal

--- app/src/main/java/com/example/e_com/MainActivity.java
+++ app/src/main/java/com/example/e_com/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.e_com.adapter.CategoryAdapter;
 import com.example.e_com.model.Category;
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView categoryRecycler;
     CategoryAdapter categoryAdapter;
+    ImageView hot_deal;
+    private MediaPlayer hot_deal_sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +33,25 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(3, "Xiaomi"));
 
         setCategoryRecycler(categoryList);
+
+        hot_deal = (ImageView) findViewById(R.id.imageView);
+        hot_deal_sound = MediaPlayer.create(this, R.raw.fire);
+        imageClick();
+    }
+    public void imageClick(){
+        hot_deal.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        soundPlay(hot_deal_sound);
+                    }
+                }
+        );
     }
 
+    public void soundPlay(MediaPlayer sound){
+        sound.start();
+    }
     private void setCategoryRecycler(List<Category> categoryList) {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
 
